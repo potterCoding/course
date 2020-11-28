558c9767689b6a73ef2741707f8c1f167dcb31f1
reminis <2949079311@qq.com>
2020-11-28 12:21:43 +0800

    1.集成代码生成器generator

--- server/src/main/java/com/reminis/server/service/TestService.java
+++ server/src/main/java/com/reminis/server/service/TestService.java
@@ -14,7 +14,7 @@ public class TestService {
     private TestMapper testMapper;
 
     public List<Test> list() {
-        return testMapper.list();
+        return testMapper.selectByExample(null);
     }
 
 }
