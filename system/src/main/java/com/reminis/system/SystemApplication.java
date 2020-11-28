51d7fb3be553c0df82fad64f0bf7d6a33f463c34
reminis <2949079311@qq.com>
2020-11-28 09:58:56 +0800

    新增子模块System

--- system/src/main/java/com/reminis/system/SystemApplication.java
+++ system/src/main/java/com/reminis/system/SystemApplication.java
@@ -1,25 +1,25 @@
-package com.reminis.course;
+package com.reminis.system;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
-@EnableEurekaServer
-public class EurekaApplication {
+@EnableEurekaClient
+public class SystemApplication {
 
-    private static final Logger LOG = LoggerFactory.getLogger(EurekaApplication.class);
+    private static final Logger LOG = LoggerFactory.getLogger(SystemApplication.class);
 //    public static void main(String[] args) {
 //        SpringApplication.run(EurekaApplication.class, args);
 //    }
 
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(EurekaApplication.class);
+        SpringApplication app = new SpringApplication(SystemApplication.class);
         ConfigurableEnvironment environment = app.run(args).getEnvironment();
         LOG.info("启动成功！！！");
-        LOG.info("Eureka地址：\thttp://127.0.0.1:{}",environment.getProperty("server.port"));
+        LOG.info("System地址：\thttp://127.0.0.1:{}",environment.getProperty("server.port"));
     }
 }
