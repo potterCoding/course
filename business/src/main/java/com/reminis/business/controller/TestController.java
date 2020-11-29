5b3ac9d789150596ad6e7ee2a4818fa7a87184ac
reminis <2949079311@qq.com>
2020-11-29 12:06:45 +0800

    1.增加子模块business 2.添加大章表

---
+++ business/src/main/java/com/reminis/business/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.reminis.business.controller;
+
+import com.reminis.server.domain.Test;
+import com.reminis.server.service.TestService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+public class TestController {
+
+    @Resource
+    private TestService testService;
+
+    @GetMapping("/test")
+    public List<Test> test() {
+        return testService.list();
+    }
+
+}
